e16a6eb91051518c25c63e44c771a18962c4571c
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2024-06-09 18:00:05 +0900

    chore: Remove unused GEMINI_MODEL environment variable

--- template_container.sh
+++ template_container.sh
@@ -4,7 +4,6 @@ podman run \
         --detach \
         --restart=always \
         --publish=8501:8501 \
-        --env=GEMINI_MODEL=gemini-1.5-flash \
         --env=GEMINI_API_KEY= \
         --name=gemini-vision-ui \
         gemini-vision-ui:20240101
--- template_script.sh
+++ template_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export GEMINI_MODEL=gemini-1.5-flash
 export GEMINI_API_KEY=
 
 streamlit run src/vision-ui.py \
