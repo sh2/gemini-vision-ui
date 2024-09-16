642209a396ccaade9e0da1576f259a01a832e7d6
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2024-09-16 20:31:44 +0900

    feat: Add support for pasting images from clipboard in Gemini Vision UI

--- src/vision-ui.py
+++ src/vision-ui.py
@@ -1,7 +1,11 @@
-import PIL.Image
+import base64
 import google.generativeai
+import io
 import os
 import streamlit as st
+import PIL.Image
+
+from st_img_pastebutton import paste as paste_image
 
 
 def main():
@@ -28,17 +32,26 @@ def main():
 
     gemini_model = google.generativeai.GenerativeModel(model_code)
 
-    files = st.file_uploader(
-        "Please upload image files", accept_multiple_files=True, type=["jpeg", "jpg", "png"])
+    upload_method = st.radio("Select upload method", [
+                             "Upload image files", "Paste from Clipboard"])
 
-    if files:
-        # Display image files in two columns
-        columns = st.columns(2)
-        column_index = 0
+    if upload_method == "Upload image files":
+        files = st.file_uploader(
+            "Please upload image files", accept_multiple_files=True, type=["jpeg", "jpg", "png"])
 
-        for file in files:
-            columns[column_index].image(file)
-            column_index = (column_index + 1) % 2
+        if files:
+            # Display image files in two columns
+            columns = st.columns(2)
+            column_index = 0
+
+            for file in files:
+                columns[column_index].image(file)
+                column_index = (column_index + 1) % 2
+    else:
+        pasted_image = paste_image("Paste from Clipboard")
+
+        if pasted_image:
+            st.image(pasted_image)
 
     # Accept messages from the user
     if prompt := st.chat_input("Please enter a question about the images"):
@@ -52,13 +65,22 @@ def main():
         with st.chat_message("assistant"):
             message_assiatant = st.empty()
 
-        if files:
-            images = [PIL.Image.open(file) for file in files]
+        images = []
 
-            # Large images are resized to 2048x2048
-            for image in images:
-                image.thumbnail((2048, 2048))
+        if upload_method == "Upload image files":
+            if files:
+                images = [PIL.Image.open(file) for file in files]
 
+                # Large images are resized to 2048x2048
+                for image in images:
+                    image.thumbnail((2048, 2048))
+        else:
+            if pasted_image:
+                image_binary = base64.b64decode(pasted_image.split(",")[1])
+                image = PIL.Image.open(io.BytesIO(image_binary))
+                images = [image]
+
+        if images:
             prompts = []
             prompts.append(prompt)
             prompts.extend(images)
