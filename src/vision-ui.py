092c6efb1fa2e3d3153da343a65ca3520815ef9b
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2024-09-29 15:04:01 +0900

    Update language model options in Gemini Vision UI

--- src/vision-ui.py
+++ src/vision-ui.py
@@ -20,9 +20,12 @@ def main():
     model_options = [
         "gemini-1.5-pro",
         "gemini-1.5-flash",
+        "gemini-1.5-pro-latest",
+        "gemini-1.5-flash-latest",
         "gemini-1.5-pro-exp-0827",
         "gemini-1.5-flash-exp-0827",
-        "gemini-1.5-flash-8b-exp-0827"
+        "gemini-1.5-flash-8b-exp-0827",
+        "gemini-1.5-flash-8b-exp-0924"
     ]
 
     model_code = st.selectbox("Language Model", model_options)
