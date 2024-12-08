b3dc51f7b7ba5fbcef12d40f34c6b7e160601a6a
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2024-12-08 09:39:10 +0900

    feat: Update requirements and add package list for dependencies

---
+++ src/memo.md
@@ -0,0 +1,7 @@
+# Package List
+
+- Pillow
+- google.generativeai
+- requests[socks]
+- st_img_pastebutton
+- streamlit
--- src/vision-ui.py
+++ src/vision-ui.py
@@ -24,12 +24,8 @@ def main():
         "gemini-1.5-pro-latest",
         "gemini-1.5-flash-latest",
         "gemini-1.5-flash-8b-latest",
-        "gemini-exp-1114",
         "gemini-exp-1121",
-        "gemini-1.5-pro-exp-0827",
-        "gemini-1.5-flash-exp-0827",
-        "gemini-1.5-flash-8b-exp-0827",
-        "gemini-1.5-flash-8b-exp-0924"
+        "gemini-exp-1206"
     ]
 
     model_code = st.selectbox("Language Model", model_options, index=7)
