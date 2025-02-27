23c6182135907728d76c947d87e704c08e8bbf71
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2025-02-27 19:16:17 +0900

    feat: Add new language model option 'gemini-2.0-flash-lite' to vision UI

--- src/vision-ui.py
+++ src/vision-ui.py
@@ -19,10 +19,10 @@ def main():
 
     model_options = [
         "gemini-2.0-flash",
+        "gemini-2.0-flash-lite",
         "gemini-1.5-pro",
         "gemini-1.5-flash",
         "gemini-1.5-flash-8b",
-        "gemini-2.0-flash-lite-preview-02-05",
         "gemini-2.0-flash-thinking-exp-01-21",
         "gemini-2.0-pro-exp-02-05",
         "gemini-2.0-flash-exp"
